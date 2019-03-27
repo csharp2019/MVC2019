560f576f07b7e7e89820aa129b52a1b524c7a18e
pmrvic <mrvic.predrag@gmail.com>
2019-03-27 18:46:48 +0100

    Primjer MVC

---
+++ HelloWorld/Controllers/PozdravSvijeteController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HelloWorld.Controllers
+{
+    public class PozdravSvijeteController : Controller
+    {
+        // GET: PozdravSvijete
+        public ActionResult Index()
+        {
+            string model = "Pozdrav svijete lalala";
+            return View((object)model);
+        }
+    }
+}
