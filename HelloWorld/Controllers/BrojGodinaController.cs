6ce261493f65a47562ac9d2a2e31a1e05a32f34e
pmrvic <mrvic.predrag@gmail.com>
2019-04-01 18:22:58 +0200

    brojgodina

---
+++ HelloWorld/Controllers/BrojGodinaController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HelloWorld.Controllers
+{
+    public class BrojGodinaController : Controller
+    {
+        // GET: BrojGodina
+        public ActionResult RacunajBrojGodina()
+        {
+            return View();
+        }
+
+        // POST /BrojGodina/RacunajBrojGodina
+        [HttpPost]
+        public ActionResult RacunajBrojGodina(DateTime Datum)
+        {
+            int razlikaGodina = (int)((DateTime.Now - Datum).TotalDays/365);
+           // int razlikaGodina= DateTime.Now.Year.
+            return View((object)razlikaGodina);
+        }
+    }
+}
---
+++ HelloWorld/Controllers/TocanOdgovorController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HelloWorld.Controllers
+{
+    public class TocanOdgovorController : Controller
+    {
+        // GET: TocanOdgovor
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult provjeri(string Odgovor)
+        {
+            string rezultat = "";
+            switch (Odgovor)
+            {
+                case "crvena":
+                case "crna":
+                case "plava": rezultat = "Odgovor je netočan"; break;
+                case "zelena": rezultat = "Odgovor je točan"; break;
+            }
+
+            return View("Index", (object)rezultat);
+        }
+    }
+}
