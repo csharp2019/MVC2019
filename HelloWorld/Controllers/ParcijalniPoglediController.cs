739fea2dc15a529e6ac5105ca526908c91edbef7
pmrvic <mrvic.predrag@gmail.com>
2019-04-02 18:31:34 +0200

    Parcijalni pogledi artikl

---
+++ HelloWorld/Controllers/ParcijalniPoglediController.cs
@@ -0,0 +1,24 @@
+﻿using HelloWorld.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HelloWorld.Controllers
+{
+    public class ParcijalniPoglediController : Controller
+    {
+        // GET: ParcijalniPogledi
+        public ActionResult PrikaziKosaricu()
+        {
+            List<Artikal> ListaArtikala = new List<Artikal>()
+            {
+ new Artikal(){Cijena=9.99m, Kategorija="plava", Kolicina=5, Naziv="Keks"},
+  new Artikal(){Cijena=8.99m, Kategorija="zelena", Kolicina=7, Naziv="Kifla"},
+   new Artikal(){Cijena=5.99m, Kategorija="zuta", Kolicina=20, Naziv="Kroki"}
+            };
+            return View(ListaArtikala);
+        }
+    }
+}
