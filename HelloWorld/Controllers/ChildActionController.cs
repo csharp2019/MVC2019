8f9de93c1b92c76fee25e3228b72aa167dc86974
pmrvic <mrvic.predrag@gmail.com>
2019-04-03 16:52:48 +0200

    html helperi

---
+++ HelloWorld/Controllers/ChildActionController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HelloWorld.Controllers
+{
+    public class ChildActionController : Controller
+    {
+        // GET: ChildAction
+        public ViewResult ChildActionView()
+        {
+            string[] proizvodi = new string[]
+            {
+                "Banana","Jabuka","Kivi","Mrkva","Kupus"
+            };
+            return View(proizvodi);
+        }
+        // GET: /ChildAction/OdrediGrupuProizvoda
+        [ChildActionOnly]
+        public string OdrediGrupuProizvoda(string proizvod)
+        {
+            switch (proizvod)
+            {
+                case "Jabuka":
+                case "Kivi":
+                case "Banana": return "voće"; 
+                default:
+                    return "povrće";
+                    
+            }
+        }
+    }
+}
