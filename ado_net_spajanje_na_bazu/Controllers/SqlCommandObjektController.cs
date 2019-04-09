a75e5cdbd1ea3afee91fb457091b7202f0817bd7
pmrvic <mrvic.predrag@gmail.com>
2019-04-09 17:23:48 +0200

    edit dbalgebra tecajevi

---
+++ ado_net_spajanje_na_bazu/Controllers/SqlCommandObjektController.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ado_net_spajanje_na_bazu.Controllers
+{
+    public class SqlCommandObjektController : Controller
+    {
+        // GET: SqlCommandObjekt
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Create()
+        {
+            // Prvo kreiramo conn string
+            // string connString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=dbAlgebra;Integrated Security=True";
+            string connString = ConfigurationManager.ConnectionStrings["dbAlgebraConnString"].ConnectionString;
+            // Nakon toga instanca Sqlconnection
+            using (SqlConnection conn = new SqlConnection(connString))
+            {
+                string cmdTxt = "";
+                cmdTxt += "INSERT INTO [dbo].[tbltecajevi] ( [naziv], [opis]) " +
+                    "VALUES ('Web design','Naucite kreirati web stranice')";
+
+                SqlCommand cmd = new SqlCommand(cmdTxt, conn);
+                cmd.Connection.Open();
+
+                int brojRedaka = cmd.ExecuteNonQuery();
+
+                if (brojRedaka > 0)
+                {
+                    ViewBag.Message = "Zapis je u bazi!";
+                }
+                else
+                {
+                    ViewBag.Message = "Zapis je u bazi!";
+                }
+            }
+            return View("Index");
+        }
+        public ActionResult Edit()
+        {
+            // Prvo kreiramo conn string
+            // string connString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=dbAlgebra;Integrated Security=True";
+            string connString = ConfigurationManager.ConnectionStrings["dbAlgebraConnString"].ConnectionString;
+            // Nakon toga instanca Sqlconnection
+            using (SqlConnection conn = new SqlConnection(connString))
+            {
+                string cmdTxt = "";
+                cmdTxt += "UPDATE [dbo].[tbltecajevi] "
+                    +"  SET " 
+                    +"[naziv] = 'Web Dev',"
+                    +"[opis] ='Web development' "
+                    + " WHERE [dbo].[tbltecajevi].id=1";
+
+                SqlCommand cmd = new SqlCommand(cmdTxt, conn);
+                cmd.Connection.Open();
+
+                int brojRedaka = cmd.ExecuteNonQuery();
+
+                if (brojRedaka > 0)
+                {
+                    ViewBag.Message = "Zapis je izmjenjen u bazi!";
+                }
+                else
+                {
+                    ViewBag.Message = "Zapis nije pronadjen ili nije izmjenjen";
+                }
+            }
+            return View("Index");
+        }
+    }
+}
---
+++ dbo.tbltecajevi.data.sql
@@ -0,0 +1,3 @@
+﻿SET IDENTITY_INSERT [dbo].[tbltecajevi] ON
+INSERT INTO [dbo].[tbltecajevi] ([id], [naziv], [opis]) VALUES (NULL, NULL, NULL)
+SET IDENTITY_INSERT [dbo].[tbltecajevi] OFF
