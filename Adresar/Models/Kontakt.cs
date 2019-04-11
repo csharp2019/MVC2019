bc1288f22c41ade0c469c444fccab2ed33402bd0
pmrvic <mrvic.predrag@gmail.com>
2019-04-11 13:40:58 +0200

    dodana klasa Kontakt

---
+++ Adresar/Models/Kontakt.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Adresar.Models
+{
+    public class Kontakt
+    {
+        public int Id { get; set; }
+        public string Ime { get; set; }
+        public string Prezime { get; set; }
+        public string Adresa { get; set; }
+        public string  Telefon { get; set; }
+        public string Mobitel { get; set; }
+        public string Email { get; set; }
+        public string Napomena { get; set; }
+        public bool Aktivan { get; set; }
+    }
+}
