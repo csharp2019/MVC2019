6a939a9d89c40d07750507f57a8c37ad48668e63
pmrvic <mrvic.predrag@gmail.com>
2019-04-11 16:46:52 +0200

    Commit prije database update

---
+++ Adresar/Models/Kontakt.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Adresar.Models
+{
+    public class Kontakt
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(25,ErrorMessage ="Ime ne smije biti vece od 25 znakova")]
+        public string Ime { get; set; }
+
+        [StringLength(25)]
+        public string Prezime   { get; set; }
+
+        [StringLength(100)]
+        public string Adresa { get; set; }
+
+        [StringLength(20)]
+        public string Telefon { get; set; }
+
+        [StringLength(20)]
+        public string Mobitel { get; set; }
+
+        [StringLength(30)]
+        public string Email { get; set; }
+
+        public string Napomena { get; set; }
+
+        public bool Aktivan { get; set; }
+    }
+}
