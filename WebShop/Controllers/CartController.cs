a213d323863268ceb2691e6cd2b6c048f5fe7f84
pmrvic <mrvic.predrag@gmail.com>
2019-04-12 18:28:07 +0200

    cart gotov

--- WebShop/Controllers/CartController.cs
+++ WebShop/Controllers/CartController.cs
@@ -22,5 +22,25 @@ namespace WebShop.Controllers
 
             return View(lstProizvodi);
         }
+        // GET: Cart/AddToCart/{id}
+        public ActionResult AddToCart(int id)
+        {
+            /*
+            if (Session["Cart"] != null)
+            {
+                lstProizvodi = Session["Cart"] as List<Proizvodi>;
+            }
+            */
+            Proizvodi proizvodi = db.Proizvodis.Find(id);
+            lstProizvodi.Add(proizvodi);
+            Session["Cart"] = lstProizvodi;
+            if(proizvodi == null)
+            {
+                return HttpNotFound("greska:Proizvod nije nadjen");
+            }
+            var proi = db.Proizvodis.Include(p => p.MjereProizvoda);
+            return RedirectToAction("Index", "WebShop", proi.ToList()); //proizvodi.ToList());
+            //return View(lstProizvodi);
+        }
     }
 }
