1d93aba5a2f41753c05bf49bdc3f631d36813b1d
pmrvic <mrvic.predrag@gmail.com>
2019-04-12 17:52:35 +0200

    napravljen cart controller / index

---
+++ WebShop/Controllers/CartController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using System.Web.Mvc;
+using WebShop.Models;
+
+namespace WebShop.Controllers
+{
+    public class CartController : Controller
+    {
+        private WebShopEntities db = new WebShopEntities();
+        public static List<Proizvodi> lstProizvodi = new List<Proizvodi>();
+        // GET: Cart
+        public ActionResult Index()
+        {
+            if (Session["Cart"] != null)
+            {
+                lstProizvodi = Session["Cart"] as List<Proizvodi>;
+            }
+
+            return View(lstProizvodi);
+        }
+    }
+}
---
+++ WebShop/Controllers/WebShopController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using System.Web.Mvc;
+using WebShop.Models;
+
+namespace WebShop.Controllers
+{
+    public class WebShopController : Controller
+    {
+        private WebShopEntities db = new WebShopEntities();
+        // GET: WebShop
+        public ActionResult Index()
+        {
+            var proizvodi = db.Proizvodis.Include(p => p.MjereProizvoda);
+            return View(proizvodi.ToList());
+        }
+    }
+}
